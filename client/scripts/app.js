b30f55c841cf4e8f505de13eccdefed1b2a67199
MKS-students <mks.students@hackreactor.com>
2015-07-10 20:33:00 -0700

    cleaned data with escape added rooms to dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,47 @@
 // YOUR CODE HERE:
 
+// TODO:
+/*
+Get create room button to work
+Filter messages by room - case insensitive
+Finish app.init
+adding friends
+CSS - materialize
+*/
 var app;
 
 $(function(){
   app = {
-
+    server: 'https://api.parse.com/1/classes/chatterbox',
     username: "hiii",
-    rooms:["lobby"],
+    rooms:{"lobby":"lobby"},
+    messages: [],
     init: function(){
-      console.log("init");},
+      // Fetch all of the Data - place in an array of objects
+      // Build roomList
+      console.log(app.rooms);
+      for (var key in app.rooms){
+        app.addRoom(key);
+      }
+      // Display messages
+      console.log("init");
+    },
     fetch: function(){
       $.ajax({
         // This is the url you should use to communicate with the parse API server.
+        url: this.server,
         type: 'GET',
         data: JSON.stringify(message), // data: {order: "-createdAt"},
         contentType: 'application/json',
         success: function (data) {
-          console.log('chatterbox: Message sent');
+          console.log(data.results);
+          // console.log(_.pluck(data.results,"roomName"));
+          app.messages = data.results;
+          for(var i=0; i<data.results.length; i++){
+            app.rooms[data.results[i].roomName] = data.results[i].roomName;
+            // console.log(_.pluck(data.results[i],"roomname"));
+            console.log(data.results[i]);
+          }
         },
         error: function (data) {
           // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -28,7 +53,7 @@ $(function(){
       console.log(message);
       $.ajax({
         // This is the url you should use to communicate with the parse API server.
-        url: 'https://api.parse.com/1/classes/chatterbox',
+        url: this.server,
         type: 'POST',
         data: JSON.stringify(message),
         contentType: 'application/json',
@@ -42,10 +67,16 @@ $(function(){
 
       });
     },
-    display: function(){},
-    refresh:function(){},
-    escape: function(){},
-    login: function(){},
+    displayMessages: function(){ // filter by room
+      console.log("displaying", this.messages.length);
+      for(var i = 0; i< this.messages.length; i++){
+        this.addMessage(this.messages[i]);
+      }
+    },
+    refresh:function(){
+      this.clearMessages();
+      this.displayMessages();
+    },
     clearMessages: function(){
       $("#chats").empty();
     },
@@ -57,29 +88,27 @@ $(function(){
       $username.addClass("username");
       var $message = $(document.createElement('p'));
       $message.addClass("message");
-      $message.html(message.text);
+      $message.html(_.escape(message.text));
       $container.append($username);
       $container.append($message);
       $('#chats').append($container);
-
-
+      console.log("addMessage");
     },
     addRoom: function(roomName){
       if (_.indexOf(this.rooms,roomName) < 1){
         $('#roomSelect').append('<option value='+roomName+'>'+roomName+'</option>');
-        this.rooms.push(roomName);
+        app.rooms[roomName] = roomName;
       }
     },
     addFriend: function(){},
-    handleSubmit: function(){}
-
-
-
-
+    handleSubmit: function(){
+      var message = {
+        username: app.username,
+        text: $('#message').val(),
+        roomname: $('#roomSelect').val()
+      };
+      app.send(message);
+      app.refresh();
+    }
   };
-
-
-
-
-
 }())
